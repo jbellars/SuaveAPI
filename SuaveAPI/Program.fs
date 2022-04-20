58947f158eddb2cce3ee73e2823161ec0edfce66
Justin Bellars <jbellars@gmail.com>
2022-04-19 17:02:48 -0700

    added service, repo, and program files

---
+++ SuaveAPI/Program.fs
@@ -0,0 +1,18 @@
+﻿namespace SuaveAPI
+module Program =
+    open Suave.Web
+    open SuaveAPI.UserService
+    open SuaveAPI.UserRepository
+    [<EntryPoint>]
+    let main argv =
+        let userActions =
+            handle
+                "users"
+                { ListUsers = UserRepository.getUsers
+                  GetUser = UserRepository.getUser
+                  AddUser = UserRepository.createUser
+                  UpdateUser = UserRepository.updateUser
+                  UpdateUserById = UserRepository.updateUserById
+                  DeleteUser = UserRepository.deleteUser }
+        startWebServer defaultConfig userActions
+        0
